9818d2fc6a442fef2dfd5a2b4c7d006afbf6fcb9
Ogbonna Alice <58136392+Alice-Vyb@users.noreply.github.com>
2020-08-07 18:00:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,59 @@
- var h1 = document.getElementsByTagName('h1')[0],
-    start = document.getElementById('start'),
-    stop = document.getElementById('stop'),
-    reset = document.getElementById('reset'),
-    lapsContainer = document.querySelector('.laps'),
-    seconds = 0, minutes = 0, hours = 0,
-    t;
+ var ms = 0, s = 0, m = 0 ;
+var timer;
 
-function add() {
-    seconds++;
-    if(seconds >= 60){
-        seconds = 0;
-        minutes++;
-        if(minutes >= 60){
-            minutes = 0;
-            hours++;
-            
+var h1 = document.querySelector('.display');
+var lapHold = document.querySelector('.lapholder');
+
+function startW() {
+    h1.textContent = timeRecorder();
+    ms++;
+
+    if(ms / 100 === 1){
+        ms = 0;
+        s++;
+        
+        if(s / 60 === 1){
+            s = 0;
+            m++;
         }
     }
-
-   h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
-
-
-    timer();
-}
-function timer() {
-    t = setTimeout(add, 100);
-}
-timer();
-
-
-/* Start button */
-start.onclick = timer;
-
-/* Stop button */
-stop.onclick = function() {
-    clearTimeout(t);
 }
 
-/* Clear button */
-reset.onclick = function() {
-    h1.textContent = "00:00:00";
-    seconds = 0; minutes = 0; hours = 0;
+function start() {
+    if(!timer) {
+        timer = setInterval(startW, 10);
+    }
+
 }
+function pause() {
+    counterStop();
+}
+
+function reset() {
+    counterStop();
+    ms = 0;
+    s = 0;
+    m = 0;
+    h1.textContent = timeRecorder();
+}
+
+function counterStop() {
+    clearInterval(timer);
+    timer = false;
+}
+
+function timeRecorder() {
+    return (m < 10 ? "0" + m : m) + ":" + (s < 10 ? "0" + s : s) + ":" + (ms < 10 ? "0" + ms : ms);
+}
+
 function laps() {
-    if(timer()){
+    if(timer){
         var li = document.createElement('li');
-        li.innerText = h1.textContent;
-        lapsContainer.appendChild(li);
+        li.innerText = timeRecorder();
+        lapHold.appendChild(li);
     }
 }
 
+function resetlap() {
+    lapHold.innerHTML = "";
+}
